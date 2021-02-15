d4e99b7bbbc99f2485e4cc2037d7ffe363ff31c1
Squarerootofpi <squarerootsofpi@gmail.com>
2021-02-15 14:17:10 -0700

    added project

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Text;
+using Newtonsoft.Json;
+using EllipticCurve;
+
+
+namespace steedcoin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrivateKey key1 = new PrivateKey();
+            PublicKey wallet1 = key1.publicKey();
+
+            PrivateKey key2 = new PrivateKey();
+            PublicKey wallet2 = key2.publicKey();
+
+
+            BlockChain steedcoin = new BlockChain(6, 100);
+
+            Console.WriteLine("start the miner.");
+            steedcoin.MinePendingTransactions(wallet1);
+            decimal bal1 = steedcoin.GetBalanceInWallet(wallet1);
+            Console.WriteLine(bal1);
+
+            //steedcoin.AddBlock(new Block(1, DateTime.Now.ToString("yyyyMMddHHmmssffff"), "amount: 50"));
+            //steedcoin.AddBlock(new Block(2, DateTime.Now.ToString("yyyyMMddHHmmssffff"), "amount: 200"));
+            Transaction tx1 = new Transaction(wallet1, wallet2, 10);
+            tx1.SignTransaction(key1);
+            steedcoin.addPendingTransaction(tx1);
+            Console.WriteLine("Stating the miner for real.");
+            steedcoin.MinePendingTransactions(wallet2);
+            Console.WriteLine("\nBalance of wallet1 is: " + steedcoin.GetBalanceInWallet(wallet1).ToString());
+            Console.WriteLine("\nBalance of wallet2 is: " + steedcoin.GetBalanceInWallet(wallet2).ToString());
+
+
+            string blockJson = JsonConvert.SerializeObject(steedcoin, Formatting.Indented);
+            Console.WriteLine(blockJson);
+
+            if (steedcoin.IsValidChain()) 
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("INVALID BLOCKCHAIN");
+            }
+            Console.WriteLine(blockJson);
+        }
+    }
+
+    
+
+   
+}
---
+++ Transaction.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Text;
+using Newtonsoft.Json;
+using EllipticCurve;
+
+namespace steedcoin
+{
+    class Transaction
+    {
+        public PublicKey FromAddress { get; set; }
+        public PublicKey ToAddress { get; set; }
+        public decimal Amount { get; set; }
+        public Signature Signature { get; set; }
+        public Transaction(PublicKey from, PublicKey to, decimal amount)
+        {
+            this.FromAddress = from;
+            this.ToAddress = to;
+            this.Amount = amount;
+
+        }
+        public void SignTransaction(PrivateKey signingKdy)
+        {
+            string fromAddressDER = BitConverter.ToString(FromAddress.toDer()).Replace("-", "");
+            string signAddressDER = BitConverter.ToString(signingKdy.publicKey().toDer()).Replace("-", "");
+
+            if(fromAddressDER != signAddressDER)
+            {
+                throw new Exception("You can't sign transactionf or another wallet!");
+            }
+
+            string toHash = this.CalculateHash();
+            this.Signature = Ecdsa.sign(toHash, signingKdy);
+        }
+
+        public string CalculateHash()
+        {
+            string fromAddressDER = BitConverter.ToString(FromAddress.toDer()).Replace("-", "");
+            string toAddressDER = BitConverter.ToString(ToAddress.toDer()).Replace("-", "");
+
+            string transactionData = fromAddressDER + toAddressDER + Amount;
+            byte[] tdBytes = Encoding.ASCII.GetBytes(transactionData);
+            return BitConverter.ToString(SHA256.Create().ComputeHash(tdBytes)).Replace("-", "");
+        }
+
+        public bool IsValid()
+        {
+            if (this.FromAddress is null)
+            {
+                return true;
+            }
+            if (this.Signature is null)
+            {
+                throw new Exception("No signature in this transaction.");
+            }
+            return Ecdsa.verify(this.CalculateHash(), this.Signature, this.FromAddress);
+        }
+    }
+}
